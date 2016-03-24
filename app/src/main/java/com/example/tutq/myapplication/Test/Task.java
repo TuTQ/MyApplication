758dbd638244ff1a43daca1fa99ab0bcddf8abb2
TuTQ <tutq@rikkeisoft.com>
2016-03-24 17:50:40 +0700

    commit

--- app/src/main/java/com/example/tutq/myapplication/Test/Task.java
+++ app/src/main/java/com/example/tutq/myapplication/Test/Task.java
@@ -29,6 +29,7 @@ public class Task extends AsyncTask<Integer, Integer, Boolean> {
         handler.sendMessage(msg);// develop2
         handler.sendMessage(msg);// develop2_10
         handler.sendMessage(msg);// develop2_13
+        handler.sendMessage(msg);// develop2_14
 //        handler.sendMessage(msg);
         super.onPreExecute();
 //        ádasđs
