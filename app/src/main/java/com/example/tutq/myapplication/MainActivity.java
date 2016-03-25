a751e5409ea9a9ed8b743a27a4e8dfddf635e7a7
TuTQ <tutq@rikkeisoft.com>
2016-03-25 10:56:32 +0700

    commit

--- app/src/main/java/com/example/tutq/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tutq/myapplication/MainActivity.java
@@ -1,45 +1,25 @@
 package com.example.tutq.myapplication;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
 //import android.support.v7.app.AppCompatActivity;
 //import android.support.v7.widget.Toolbar;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 
-import com.example.tutq.myapplication.Test.FragmentC;
 import com.example.tutq.myapplication.Test.Item;
-import com.example.tutq.myapplication.Test.Task;
-import com.example.tutq.myapplication.Test.TestClass;
 
-import junit.framework.Test;
-
-import java.nio.MappedByteBuffer;
 import java.util.ArrayList;
 
 public class MainActivity extends FragmentActivity {
